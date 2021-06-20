21f7f3e17096590bf996d63ed515066d861e7679
pawelbiernacki <pawelbiernacki9605@gmail.com>
2021-06-20 22:07:53 +0200

    Detail page and 404 page added.

---
+++ Kulinarna.Web/Pages/Details.cshtml.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Kulinarna.BusinessLogic.DTOs;
+using Kulinarna.BusinessLogic.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Kulinarna.Web
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly RecipeService recipeService;
+
+        public RecipeDTO RecipeDTO { get; set; }
+
+        public DetailsModel(RecipeService recipeService)
+        {
+            this.recipeService = recipeService;
+        }
+
+        public IActionResult OnGet(int recipeID)
+        {
+            RecipeDTO = recipeService.GetRecipeById(recipeID);
+            if (RecipeDTO == null)
+            {
+                return RedirectToPage("./404");
+            }
+            return Page();
+        }
+    }
+}
