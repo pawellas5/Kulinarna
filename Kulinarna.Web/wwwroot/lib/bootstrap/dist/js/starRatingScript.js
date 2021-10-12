ed6879cb536820ce16624ae56400cc7fa12f7f94
pawelbiernacki <pawelbiernacki9605@gmail.com>
2021-10-12 19:33:50 +0200

    Star rating appearance got better thanks to starRatingScript.js that has been added to the project.

---
+++ Kulinarna.Web/wwwroot/lib/bootstrap/dist/js/starRatingScript.js
@@ -0,0 +1,79 @@
+﻿'use strict'
+
+let changeColor = function () {
+
+    let star1 = document.getElementById("star1");
+    let star2 = document.getElementById("star2");
+    let star3 = document.getElementById("star3");
+    let star4 = document.getElementById("star4");
+    let star5 = document.getElementById("star5");
+
+    //Rate 1
+    star1.addEventListener("mouseover", function () {
+
+        star1.style.color = "rgba(255,215,0)";
+
+    });
+    star1.addEventListener("mouseout", function () {
+        star1.style.color = "rgba(255, 215, 0, 0.4)"
+    });
+
+    //Rate2
+    star2.addEventListener("mouseover", function () {
+        star1.style.color = "rgba(255,215,0)";
+        star2.style.color = "rgba(255,215,0)";
+    });
+    star2.addEventListener("mouseout", function () {
+        star1.style.color = "rgba(255, 215, 0, 0.4)"
+        star2.style.color = "rgba(255, 215, 0, 0.4)"
+    });
+
+    //Rate3
+    star3.addEventListener("mouseover", function () {
+        star1.style.color = "rgba(255,215,0)";
+        star2.style.color = "rgba(255,215,0)";
+        star3.style.color = "rgba(255,215,0)";
+    });
+    star3.addEventListener("mouseout", function () {
+        star1.style.color = "rgba(255, 215, 0, 0.4)"
+        star2.style.color = "rgba(255, 215, 0, 0.4)"
+        star3.style.color = "rgba(255, 215, 0, 0.4)"
+    });
+
+    //Rate4
+    star4.addEventListener("mouseover", function () {
+        star1.style.color = "rgba(255,215,0)";
+        star2.style.color = "rgba(255,215,0)";
+        star3.style.color = "rgba(255,215,0)";
+        star4.style.color = "rgba(255,215,0)";
+    });
+    star4.addEventListener("mouseout", function () {
+        star1.style.color = "rgba(255, 215, 0, 0.4)";
+        star2.style.color = "rgba(255, 215, 0, 0.4)";
+        star3.style.color = "rgba(255, 215, 0, 0.4)";
+        star4.style.color = "rgba(255, 215, 0, 0.4)";
+    });
+
+    //Rate5
+    star5.addEventListener("mouseover", function () {
+        star1.style.color = "rgba(255,215,0)";
+        star2.style.color = "rgba(255,215,0)";
+        star3.style.color = "rgba(255,215,0)";
+        star4.style.color = "rgba(255,215,0)";
+        star5.style.color = "rgba(255,215,0)";
+    });
+    star5.addEventListener("mouseout", function () {
+        star1.style.color = "rgba(255, 215, 0, 0.4)";
+        star2.style.color = "rgba(255, 215, 0, 0.4)";
+        star3.style.color = "rgba(255, 215, 0, 0.4)";
+        star4.style.color = "rgba(255, 215, 0, 0.4)";
+        star5.style.color = "rgba(255, 215, 0, 0.4)";
+    })
+
+
+
+
+
+}
+
+changeColor();
